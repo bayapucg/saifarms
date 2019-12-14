3e9010389a5f53469a23ba8f265f1594a27a5e10
bayapucg <bayapu.edu004@gmail.com>
2019-12-14 11:45:08 +0530

    add
    project

---
+++ header.php
@@ -0,0 +1,34 @@
+<body>
+  <header id="header">
+    <div class="container-fluid">
+
+      <div id="logo" class="pull-left logo-cus">
+        <h1><a href="index.php" class="scrollto"><img src="img/logo.png" ></a></h1>
+      </div>
+
+      <nav id="nav-menu-container">
+        <ul class="nav-menu">
+         
+          <li ><a href="index.php">Home </a></li>
+          <li ><a href="about-us.php">About </a></li>
+          
+          <li class="menu-has-children">
+			<a href="service-detais.php">Services</a>
+            <ul>
+              <li><a href="service-detais.php">Breeding Rams </a></li>
+              <li><a href="service-detais.php">Ramlams </a></li>
+              <li><a href="service-detais.php">Sheep Supply </a></li>
+              <li><a href="service-detais.php">Meat Supply</a></li>
+              <li><a href="service-detais.php">Silage Grass</a></li>
+              <li><a href="service-detais.php">Live</a></li>
+             
+            </ul>
+          </li>
+		    <li ><a href="gallery.php">Gallery </a></li>
+		    <li ><a href="contactus.php">Contact Us </a></li>
+		    
+          
+        </ul>
+      </nav><!-- #nav-menu-container -->
+    </div>
+  </header>
---
+++ index.php
@@ -0,0 +1,290 @@
+<?php include("header-links.php"); ?>
+<?php include("header.php"); ?>
+
+<section id="intro">
+    <div class="row">
+    <div class="col-md-12" style="padding:0;margin:0">
+    <div class="intro-container">
+      <div id="introCarousel" class="carousel  slide carousel-fade" data-ride="carousel">
+
+        <ol class="carousel-indicators"></ol>
+
+        <div class="carousel-inner" role="listbox">
+
+       
+          <div class="carousel-item">
+            <div class="carousel-background"><img src="img/intro-carousel/2.jpg" alt=""></div>
+            <div class="carousel-container">
+              <div class="carousel-content">
+                <h2>Natural  Ethical and Sustainable <br> Sheap | Country Bird Farm</h2>
+                
+                <a href="#featured-services" class="btn-get-started scrollto">Get Started</a>
+              </div>
+            </div>
+          </div>
+		  <div class="carousel-item active">
+            <div class="carousel-background"><img src="img/intro-carousel/1.jpg" alt=""></div>
+            <div class="carousel-container">
+              <div class="carousel-content">
+                <h2>We Supply Consistently  Better Silage</h2>
+                <p>Ensuring that our products are of outstanding quality and value for money.</p>
+                <a href="#featured-services" class="btn-get-started scrollto">Get Started</a>
+              </div>
+            </div>
+          </div>
+
+        </div>
+
+        <a class="carousel-control-prev" href="#introCarousel" role="button" data-slide="prev">
+          <span class="carousel-control-prev-icon ion-chevron-left" aria-hidden="true"></span>
+          <span class="sr-only">Previous</span>
+        </a>
+
+        <a class="carousel-control-next" href="#introCarousel" role="button" data-slide="next">
+          <span class="carousel-control-next-icon ion-chevron-right" aria-hidden="true"></span>
+          <span class="sr-only">Next</span>
+        </a>
+
+      </div>
+    </div>
+    </div>
+    </div>
+  </section><!-- #intro -->
+
+  <main id="main">
+
+    <section id="about">
+      <div class="container">
+
+        <header class="section-header">
+          <h3>About Us</h3>
+          <p>   SAIFARMS Goat & Sheep Farm is situated at the outskirts of Jangamvaripalle, C.S. Puram Prakasam District, Andhrapradesh, India. The farm has been established to pioneer in identifying, developing and propogating the best goat breeds that is suitable to the conditions preventant in C.S. Puram.</p>
+        </header>
+
+        <div class="row about-cols">
+
+          <div class="col-md-4 wow fadeInUp">
+            <div class="about-col">
+              <div class="img">
+                <img src="img/about-mission.jpg" alt="" class="img-fluid">
+                <div class="icon"><i class="ion-ios-speedometer-outline"></i></div>
+              </div>
+              <h2 class="title"><a href="#">Sheap Farming </a></h2>
+              <p>
+                help with planning and accessing the best care and support
+              </p>
+            </div>
+          </div>
+
+          <div class="col-md-4 wow fadeInUp" data-wow-delay="0.1s">
+            <div class="about-col">
+              <div class="img">
+                <img src="img/about-plan.jpg" alt="" class="img-fluid">
+                <div class="icon"><i class="ion-ios-list-outline"></i></div>
+              </div>
+              <h2 class="title"><a href="#">Country Bird Farming</a></h2>
+              <p>
+                find out how rehabilitation can change lives
+              </p>
+            </div>
+          </div>
+
+          <div class="col-md-4 wow fadeInUp" data-wow-delay="0.2s">
+            <div class="about-col">
+              <div class="img">
+                <img src="img/about-vision.jpg" alt="" class="img-fluid">
+                <div class="icon"><i class="ion-ios-eye-outline"></i></div>
+              </div>
+              <h2 class="title"><a href="#">Goat Farming </a></h2>
+              <p>
+                Our factsheets provide information on benefits you may be entitled to following an injury
+              </p>
+            </div>
+          </div>
+
+        </div>
+
+      </div>
+    </section><!-- #about -->
+
+    <!--==========================
+      Services Section
+    ============================-->
+    <section id="services">
+      <div class="container">
+
+        <header class="section-header wow fadeInUp">
+          <h3>Services</h3>
+          <p>If you’ve suffered as a result of medical malpractice we’re here for you. As the One and Only clinical and medical negligence help line in the India, we know how to make a positive difference to your life. Our experienced medical negligence Clinicians & legal experts will take the time to understand what you’re going through, helping you get the answers and compensation you deserve.</p>
+        </header>
+
+        <div class="row">
+
+         
+          <div class="col-lg-4 col-md-6 box wow bounceInUp" data-wow-duration="1.4s">
+            <div class="icon"><i class="ion-android-cloud-done"></i></div>
+            <h4 class="title"><a href="">Breeding Rams</a></h4>
+            <p class="description">Minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat tarad limino ata</p>
+          </div>
+          <div class="col-lg-4 col-md-6 box wow bounceInUp" data-wow-duration="1.4s">
+            <div class="icon"><i class="ion-android-cloud-done"></i></div>
+            <h4 class="title"><a href="">Ramlams</a></h4>
+            <p class="description">Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur</p>
+          </div>
+          <div class="col-lg-4 col-md-6 box wow bounceInUp" data-wow-delay="0.1s" data-wow-duration="1.4s">
+            <div class="icon"><i class="ion-android-cloud-done"></i></div>
+            <h4 class="title"><a href="">Sheep Supply</a></h4>
+            <p class="description">Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
+          </div>
+          <div class="col-lg-4 col-md-6 box wow bounceInUp" data-wow-delay="0.1s" data-wow-duration="1.4s">
+            <div class="icon"><i class="ion-android-cloud-done"></i></div>
+            <h4 class="title"><a href="">Meat Supply</a></h4>
+            <p class="description">At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque</p>
+          </div>
+          <div class="col-lg-4 col-md-6 box wow bounceInUp" data-wow-delay="0.1s" data-wow-duration="1.4s">
+            <div class="icon"><i class="ion-android-cloud-done"></i></div>
+            <h4 class="title"><a href="">Silage Grass</a></h4>
+            <p class="description">Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi</p>
+          </div>
+		  <div class="col-lg-4 col-md-6 box wow bounceInUp" data-wow-delay="0.1s" data-wow-duration="1.4s">
+            <div class="icon"><i class="ion-android-cloud-done"></i></div>
+            <h4 class="title"><a href="">Live</a></h4>
+            <p class="description">Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi</p>
+          </div> 
+
+        </div>
+		<div class="row">
+			<div class="col-md-6">
+				<video width="100%"  controls muted autoplay>
+				  <source src="video/sheap.mp4" type="video/mp4">
+				  <source src="video/sheap.ogg" type="video/ogg">
+				</video>
+			</div>
+			<div class="col-md-6">
+					<video width="100%"  controls muted autoplay>
+			  <source src="video/chiken.mp4" type="video/mp4">
+			  <source src="video/chiken.ogg" type="video/ogg">
+			  
+			</video>
+			</div>
+		</div>
+
+      </div>
+    </section><!-- #services -->
+
+    <!--==========================
+      Call To Action Section
+    ============================-->
+    <section id="call-to-action" class="wow fadeIn">
+      <div class="container text-center">
+        <h3>Call To Action</h3>
+        <p> +91 97054 74959 | +91 70328 68969 | +91 94903 73838 (or) saisheepfarm7@gmail.com</p>
+      
+      </div>
+    </section>
+    <section id="facts"  class="wow fadeIn">
+      <div class="container">
+
+        <header class="section-header">
+          <h3>Facts</h3>
+          <p>Agrienvironmental schemes support conversion from conventional towards organic farming. Only few know how difficult organic sheep and goat farming is from animal welfare, ecological and economic perspective. Newcomers particularly overestimate the
+production and marketing potential of the field, and underestimate the associated husbandry </p>
+        </header>
+
+        <div class="row counters">
+
+  				<div class="col-lg-3 col-6 text-center">
+            <span data-toggle="counter-up">10000 </span>
+            <p>Sheaps</p>
+  				</div>
+
+          <div class="col-lg-3 col-6 text-center">
+            <span data-toggle="counter-up">8000 </span>
+            <p>Goats</p>
+  				</div>
+
+          <div class="col-lg-3 col-6 text-center">
+            <span data-toggle="counter-up">20000 </span> 
+            <p>Country Birds</p>
+  				</div>
+
+          <div class="col-lg-3 col-6 text-center">
+            <span data-toggle="counter-up">50 </span> 
+            <p>Hard Workers</p>
+  				</div>
+
+  			</div></div>
+    </section>
+	  <section id="testimonials" class="section-bg wow fadeInUp">
+      <div class="container">
+
+        <header class="section-header">
+          <h3>Testimonials</h3>
+        </header>
+
+        <div class="owl-carousel testimonials-carousel">
+
+          <div class="testimonial-item">
+            <img src="img/testimonial-1.jpg" class="testimonial-img" alt="">
+            <h3>Srinivasa Reddy</h3>
+            <h4>Reddy Meat Store</h4>
+            <p>
+              <img src="img/quote-sign-left.png" class="quote-sign-left" alt="">
+              Sai meat farms is a great place to work. It was organized and everyone got along. Goats and Sheap is very healthy and active.
+              <img src="img/quote-sign-right.png" class="quote-sign-right" alt="">
+            </p>
+          </div>
+
+          <div class="testimonial-item">
+            <img src="img/testimonial-2.jpg" class="testimonial-img" alt="">
+            <h3>Chandu</h3>
+            <h4>Chandu Chicken & Mutton Center</h4>
+            <p>
+              <img src="img/quote-sign-left.png" class="quote-sign-left" alt="">
+             Thanks for Sai Farms, Really am so happy for joined with Sai Farms. They provide on time and health Goats and Sheap with Organic process
+              <img src="img/quote-sign-right.png" class="quote-sign-right" alt="">
+            </p>
+          </div>
+
+        </div>
+
+      </div>
+    </section>
+    <section id="team">
+      <div class="container">
+        <div class="section-header wow fadeInUp">
+          <h3>Founder</h3>
+        
+        </div>
+
+        <div class="row">
+
+          <div class="col-lg-3 col-md-6 wow fadeInUp">
+			<img src="img/founder.png" class="img-fluid img-thumbnail" alt="">
+          </div>
+		  <div class="col-lg-9 col-md-9 wow fadeInUp">
+			  <p>We Have Been Sheep Farmers In Maine Since 2013 With A Foundation Flock Of Nellore zodepi And Nellore Brown Sheep. Both Side Of Our Families Have A Long And Notable History Of Raising Sheep.</p>
+			  <h3>V.Srinivasa Reddy</h3>
+			  <h4>Saisheepfarm7@gmail.com</h4>
+			  <h4>+91 97054 74959 | +91 94903 73838 | +91 70328 68969</h4>
+			 
+          </div>
+
+          
+          
+
+          
+
+        </div>
+
+      </div>
+    </section>
+  
+	
+    
+  </main>
+  
+  <?php include("footer.php"); ?>
+<?php include("footer-links.php"); ?>
+
+  
